f2876b86c930a1b8ec474f13749f18605bc406ad
Mike Walsh <mwalsh@me.com>
2013-09-29 15:05:30 -0700

    Fix for issue #15 where on restore, all dependent child association records restored regardless of deletion date

--- lib/destroyed_at.rb
+++ lib/destroyed_at.rb
@@ -13,7 +13,7 @@ module DestroyedAt
   def destroy
     run_callbacks(:destroy) do
       destroy_associations
-      self.update_attribute(:destroyed_at, current_time_from_proper_timezone)
+      self.update_attribute(:destroyed_at, _get_destroy_time)
       @destroyed = true
     end
   end
@@ -22,9 +22,10 @@ module DestroyedAt
   def restore
     state = nil
     run_callbacks(:restore) do
+      destroyed_time = self.destroyed_at
       if state = self.update_attribute(:destroyed_at, nil)
         @destroyed = false
-        _restore_associations
+        _restore_associations destroyed_time
       end
     end
     state
@@ -32,19 +33,24 @@ module DestroyedAt
 
   private
 
+  def _get_destroy_time
+    @@destroy_time ||= current_time_from_proper_timezone
+  end
+
   def _set_destruction_state
+    @@destroy_time = nil
     @destroyed = destroyed_at.present? if has_attribute?(:destroyed_at)
     # Don't stop the other callbacks from running
     true
   end
 
-  def _restore_associations
+  def _restore_associations(destroyed_time)
     reflections.select { |key, value| value.options[:dependent] == :destroy }.keys.each do |key|
       assoc = association(key)
       if assoc.options[:through] && assoc.options[:dependent] == :destroy
         assoc = association(assoc.options[:through])
       end
-      assoc.association_scope.each { |r| r.restore if r.respond_to? :restore }
+      assoc.association_scope.each { |r| r.restore if r.respond_to?(:restore) && (r.destroyed_at.to_i == destroyed_time.to_i) }
     end
   end
 end
